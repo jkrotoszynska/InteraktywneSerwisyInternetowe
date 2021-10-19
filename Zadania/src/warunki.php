8ed84a9cfec849ee31e49812e2ddc9a5b9055f6d
Justyna Krotoszynska <justyna.krotoszynska@gmail.com>
2021-10-19 18:18:23 +0200

    zadania

---
+++ Zadania/src/warunki.php
@@ -0,0 +1,8 @@
+<div class="flex flex-row flex-wrap w-full justify-center font-bold text-3xl mb-2">
+  Warunki
+</div>
+<div class="flex flex-row flex-wrap w-full justify-center text-xl mb-2">
+  <?php
+  $liczba = 8
+  ?>
+</div>
---
+++ Zadania/src/zmienne.php
@@ -0,0 +1,14 @@
+<div class="flex flex-row flex-wrap w-full justify-center font-bold text-3xl mb-2">
+  Zmienne
+</div>
+<div class="flex flex-row flex-wrap w-full justify-center text-xl mb-2">
+  <?php
+  $zmienna = 'Hello World!';
+  echo $zmienna . "</br>";
+
+  $pierwszaLiczba = 5;
+  $drugaLiczba = 1.5;
+
+  echo $wynik = $pierwszaLiczba + $drugaLiczba
+  ?>
+</div>
