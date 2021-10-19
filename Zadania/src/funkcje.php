190f510390282f0123d8c1305907a2cfaf3f02af
Justyna Krotoszynska <justyna.krotoszynska@gmail.com>
2021-10-19 20:10:37 +0200

    update

--- Zadania/src/funkcje.php
+++ Zadania/src/funkcje.php
@@ -3,15 +3,38 @@
 </div>
 <div class="flex flex-row flex-wrap w-full justify-center text-xl mb-2">
   <?php
+
+  #zadanie 2
   function pierwszaFunkcja(){
     echo("Wiadomosc z pierwszej funkcji" . "</br>");
   }
   pierwszaFunkcja();
 
+  #zadanie 3
   function funkcjaZArgumentami($imie, $nazwisko = "Nowak"){
     echo("Hello ".$imie." ".$nazwisko . "</br>");
   }
   funkcjaZArgumentami("Jan");
   funkcjaZArgumentami("Kapcer","Szymański");
+
+  #zadanie 4
+  function funkcjaZNieznanaLiczbaArgumentow(){
+    $val = "";
+    $tab = func_get_args();
+    foreach($tab as $v){
+      $val .= $v;
+    }
+    echo ($val . "</br>");
+  }
+  funkcjaZNieznanaLiczbaArgumentow("cytyryny", "kamienie");
+  funkcjaZNieznanaLiczbaArgumentow("motylek", "fiołek");
+
+  #zadanie 5
+  function funkcjaZReferencja(&$imie){
+
+  }
+
   ?>
+
+
 </div>
--- Zadania/src/petle.php
+++ Zadania/src/petle.php
@@ -3,8 +3,30 @@
 </div>
 <div class="flex flex-row flex-wrap w-full justify-center text-xl mb-2">
   <?php
+  #zadanie 2
   for($count = 0; $count <= 10; $count++){
     echo($count);
   }
+
+  echo('</br>');
+
+  #zadanie 3
+  $i = 0;
+  while($i++ <= 10){
+    echo($i-1);
+  }
+
+  echo('</br>');
+
+  $j = 5;
+  do{
+    echo($j);
+    $j++;
+  }while($j<=10);
+
+  echo('</br>');
+
+  #zadanie 4
+
   ?>
 </div>
